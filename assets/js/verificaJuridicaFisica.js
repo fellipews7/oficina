073c5c44a5be5958e5a1a5429aebb2649953363f
NathanBergmann <78989988+NathanBergmann@users.noreply.github.com>
2021-06-11 19:59:44 -0300

    Update verificaJuridicaFisica.js

--- assets/js/verificaJuridicaFisica.js
+++ assets/js/verificaJuridicaFisica.js
@@ -1,18 +1,18 @@
 
 var divCPF = document.getElementById("divCPF");
 var divCNPJ = document.getElementById("divCNPJ");
-var opcF = document.getElementById("iFisica");
-var opcJ = document.getElementById("iJuridica");
+var ocultaPessoaFisica = document.getElementById("iFisica");
+var ocultaPessoaJuridica = document.getElementById("iJuridica");
 
 divCNPJ.style.display = 'none';
 
-opcJ.onclick = function(){
+ocultaPessoaJuridica.onclick = function(){
 
     divCPF.style.display = 'none';
     divCNPJ.style.display = 'block';
 }
 
-opcF.onclick = function(){
+ocultaPessoaFisica.onclick = function(){
     divCPF.style.display = 'block';
     divCNPJ.style.display = 'none';
 }
