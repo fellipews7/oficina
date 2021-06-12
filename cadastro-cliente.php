a251d4bed68a818e9916973a2b16291c6ae8eb6e
Douglas <douglas-cprdev@hotmail.com>
2021-06-12 18:18:43 -0300

    :hammer: fixing file references, fixing the burguer menu in responsive mode

--- cadastro-cliente.php
+++ cadastro-cliente.php
@@ -180,7 +180,12 @@
         </div>
     </div>
 </div>
+
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-    <script src="script.js"></script>
+    <script src="assets/js/script.js"></script>
   </body>
 </html>
