914ea075eed163a276818cb38bd7aca79b1681ee
NathanBergmann <nathan_bergmann@outlook.com>
2021-06-26 10:18:22 -0300

    Consulta Clientes - Adicionado a opção CHECKED no Nome; Retirado a coluna de Status da busca  |  Consulta Orçamentos - Alerado data Inicial padrão para 01/01/2021 e Alterado data Final para Buscar o dia atual; Arrumado ordem das colunas  |

--- App/Consulta-Carros.php
+++ App/Consulta-Carros.php
@@ -73,13 +73,10 @@
             <th>Ano Modelo</th>
             <th>Ano Fabricado</th>
             <th>Cliente Atual</th> 
-<<<<<<< HEAD:Consulta-Carros.php
             <th></th>
           </thead>
-=======
             <th><i class="fa fa-search-plus" aria-hidden="true"></i></th>
           </tr>
->>>>>>> 9a5a34c79899769dc758df5b388e392cb723ae36:App/Consulta-Carros.php
           <tr>
               <?php
               if(isset($_GET['nPesquisarCarroCon'])) {
