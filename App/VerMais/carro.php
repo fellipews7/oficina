efb7309958f935e21bc30f1491df4a435fb98cf5
Douglas Vinicius <douglas-cprdev@hotmail.com>
2021-06-28 21:05:46 -0300

    :rocket: mudança na tela ver mais

--- App/VerMais/carro.php
+++ App/VerMais/carro.php
@@ -7,41 +7,39 @@ require_once '../connection.php';
 <div  class="row">
     <div class="col s12 m6 push-m3">
     <a href="../Consulta-Carros.php" class="btn black">Retornar</a><h3 class="light">Carros</h3>
-        <table class="striped">
-            <thead>
-                <th>ID</th>
-                <th>Placa</th> 
-                <th>Modelo</th>
-                <th>Marca</th>
-                <th>Ano Modelo</th>
-                <th>Ano Fabricado</th>
-                <th>Renavam</th>
-            </thead>
+        <div class="responsive-table">
+            <table class="table striped">
+                <thead>
+                    <th>ID</th>
+                    <th>Placa</th> 
+                    <th>Modelo</th>
+                    <th>Marca</th>
+                    <th>Ano Modelo</th>
+                    <th>Ano Fabricado</th>
+                    <th>Renavam</th>
+                </thead>
 
-            <tbody>
-                <?php
-                    $id = $_GET['id'];
-                    $sql = "SELECT * FROM carros WHERE id = ".$id;
-                    $resultado = mysqli_query($connect, $sql);
-                    while($dados = mysqli_fetch_array($resultado)):
-                ?>
-                <tr>
-                    <td><?php echo $dados['id']?></td>
-                    <td><?php echo $dados['placa']?></td>
-                    <td><?php echo $dados['modelo']?></td>
-                    <td><?php echo $dados['marca']?></td>
-                    <td><?php echo $dados['ano_modelo']?></td>
-                    <td><?php echo $dados['ano_fabricado']?></td>
-                    <td><?php echo $dados['renavam']?></td>
-                    <td><a href="editarCarro.php?id=<?php echo $dados['id']?>" class="btn black">Editar </a></td>
-
-                    <td></td>
-
-                </tr>
-
-                <?php endwhile; ?>
-            </tbody>
-        </table>
+                <tbody>
+                    <?php
+                        $id = $_GET['id'];
+                        $sql = "SELECT * FROM carros WHERE id = ".$id;
+                        $resultado = mysqli_query($connect, $sql);
+                        while($dados = mysqli_fetch_array($resultado)):
+                    ?>
+                    <tr>
+                        <td><?php echo $dados['id']?></td>
+                        <td><?php echo $dados['placa']?></td>
+                        <td><?php echo $dados['modelo']?></td>
+                        <td><?php echo $dados['marca']?></td>
+                        <td><?php echo $dados['ano_modelo']?></td>
+                        <td><?php echo $dados['ano_fabricado']?></td>
+                        <td><?php echo $dados['renavam']?></td>
+                        <td><a href="editarCarro.php?id=<?php echo $dados['id']?>" class="btn black">Editar </a></td>
+                    </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+        </div>
         <br>
     </div>
 </div>
