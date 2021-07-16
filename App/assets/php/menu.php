ee3f704898f33581bc724994b32c895e1455e8a9
NathanBergmann <nathan_bergmann@outlook.com>
2021-07-15 22:23:09 -0300

    Retirado os Menus de Todas as telas, centralizando o menu em assets/php/menu.php  |  Arrumado a Indentação nas telas de Consulta, Cadastro e Index

--- App/assets/php/menu.php
+++ App/assets/php/menu.php
@@ -1,68 +1,65 @@
-<nav class="menu">
-    <ul>
-        <div class="dropdown">
-            <button class="dropbtn">
-                <li><span>Inicio</span></li>
-            </button>
-            <div class="dropdown-content">
-                <a href="index.php" id="iMenu">Inicio</a>
-            </div>
+<div id="sidebar">
+    <div class="sidebar__title">
+        <div class="sidebar__img">
+            <img src="assets/logo.png" alt="logo" />
+            <h2> Oficina Schulz </h2>
         </div>
-        <div class="dropdown">
-            <button class="dropbtn">
-                <li><span>Clientes</span></li>
-            </button>
-            <div class="dropdown-content">
-                <a href="cadastro_cliente.php" id="iMenu">Cadastro</a>
-                <a href="consulta_cliente.php" id="iMenu">Consulta</a>
-            </div>
+        <br>
+        <i onclick="closeSidebar()" class="fa fa-times" id="sidebarIcon" aria-hidden="true"></i>
+    </div>
+
+    <div class="sidebar__menu">
+        <div class="sidebar__link active_menu_link">
+            <i class="fa fa-home"></i>
+            <a href="index.php">Dashboard</a>
         </div>
-        <div class="dropdown">
-            <button class="dropbtn">
-                <li><span>Carros</span></li>
-            </button>
-            <div class="dropdown-content">
-                <a href="cadastro_carros.php" id="iMenu">Cadastro</a>
-                <a href="consulta_carros.php" id="iMenu">Consulta</a>
-            </div>
+        <h2>Cadastros</h2>
+        <div class="sidebar__link">
+            <i class="fa fa-user" aria-hidden="true"></i>
+            <a href="Cadastro-Cliente.php">Cadastro Cliente</a>
         </div>
-        <div class="dropdown">
-            <button class="dropbtn">
-                <li><span>Ordens de Serviço</span></li>
-            </button>
-            <div class="dropdown-content">
-                <a href="cadastro_OS.php" id="iMenu">Cadastro</a>
-                <a href="consulta_OS.php" id="iMenu">Consulta</a>
-                <a href="servicos_OS.php" id="iMenu">Serviços</a>
-            </div>
+        <div class="sidebar__link">
+            <i class="fa fa-car" aria-hidden="true"></i>
+            <a href="Cadastro-Carro.php">Cadastro Carro</a>
         </div>
-        <div class="dropdown">
-            <button class="dropbtn">
-                <li><span>Funcionários</span></li>
-            </button>
-            <div class="dropdown-content">
-                <a href="cadastro_funcionarios.php" id="iMenu">Cadastro</a>
-                <a href="cadastro_cargos_funcionarios.php" id="iMenu">Cadastro de Cargo</a>
-                <a href="consulta_funcionarios.php" id="iMenu">Consulta</a>
-            </div>
+        <div class="sidebar__link">
+            <i class="fa fa-money" aria-hidden="true"></i>
+            <a href="Cadastro-Orcamento.php">Cadastro Orçamento</a>
         </div>
-        <div class="dropdown">
-            <button class="dropbtn">
-                <li><span>Orçamentos</span></li>
-            </button>
-            <div class="dropdown-content">
-                <a href="cadastro_orcamento.php" id="iMenu">Cadastro</a>
-                <a href="consulta_orcamento.php" id="iMenu">Consulta</a>
-            </div>
+        <div class="sidebar__link">
+            <i class="fa fa-users" aria-hidden="true"></i>
+            <a href="Cadastro-Funcionario.php">Cadastro Funcionário</a>
         </div>
-        <div class="dropdown">
-            <button class="dropbtn">
-                <li><span>Produtos</span></li>
-            </button>
-            <div class="dropdown-content">
-                <a href="cadastro_produtos.php" id="iMenu">Cadastro</a>
-                <a href="consulta_produtos.php" id="iMenu">Consulta</a>
-            </div>
+        <div class="sidebar__link">
+            <i class="fa fa-briefcase" aria-hidden="true"></i>
+            <a href="Cadastro-Cargo.php">Cadastro Cargo</a>
         </div>
-    </ul>
-</nav>
+        <h2>Consultas</h2>
+        <div class="sidebar__link">
+            <i class="fa fa-user" aria-hidden="true"></i>
+            <a href="Consulta-Cliente.php">Consulta Cliente</a>
+        </div>
+        <div class="sidebar__link">
+            <i class="fa fa-car" aria-hidden="true"></i>
+            <a href="Consulta-Carros.php">Consulta Carro</a>
+        </div>
+        <div class="sidebar__link">
+            <i class="fa fa-money" aria-hidden="true"></i>
+            <a href="Consulta-Orcamento.php">Consulta Orçamento</a>
+        </div>
+        <div class="sidebar__link">
+            <i class="fa fa-sitemap" aria-hidden="true"></i>
+            <a href="Consulta-OS.php">Consulta Ordem Serviço</a>
+        </div>
+        <div class="sidebar__link">
+            <i class="fa fa-users" aria-hidden="true"></i>
+            <a href="Consulta-Funcionarios.php">Consulta Funcionario</a>
+        </div>
+        <div class="sidebar__logout">
+            <i class="fa fa-power-off"></i>
+            <?php $_SESSION['logado'] = 0; ?>><a href="../index.php">Log out</a>
+
+        </div>
+    </div>
+</div>
+</div>
